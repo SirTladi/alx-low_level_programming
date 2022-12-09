2b882c94d7a07c8b40c22fb9530bc5e6132369bc
sirtladi <kgahlishot@gmail.com>
2022-12-08 17:02:52 -0800

    task 06 sizable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+
 /**
- * main - Entry
- * Return: Always 0
+ * main - prints the size of various types 
+ * Return: 0 if exited properly, non-zero otherwise
  */
+
 int main(void)
 {
-	printf("Size of char: %i byte(s)\n", sizeof(char));
-	printf("Size of an int: %i byte(s)\n", sizeof(int));
-	printf("Size of an int: %i byte(s)\n", sizeof(long int));
-	printf("Size of an int: %i byte(s)\n", sizeof(long long int));
-	printf("Size of an int: %i byte(s)\n", sizeof(float));
+	printf("Size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
