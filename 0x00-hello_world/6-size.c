218b91da794e4e9dc4fc1c31a72bb7a1097842b1
sirtladi <kgahlishot@gmail.com>
2022-12-08 16:53:02 -0800

    task 06 size yadi 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,10 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("Size of char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of an int: %i byte(s)\n", sizeof(long int));
+	printf("Size of an int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of an int: %i byte(s)\n", sizeof(float));
 	return (0);
 }
