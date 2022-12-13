5d25fdf8ed18a70babbd04756ca6d4c7794b5e99
sirtladi <kgahlishot@gmail.com>
2022-12-13 12:53:40 -0800

    task yela ya alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,12 @@
 void print_alphabet_x10(void)
 {
 	int ten;
-	char lc;
+	char la;
 
 	for (ten = 0; ten <= 9; ten++)
 	{
-		for (lc = 'a' lc <= 'z'; lc++)
-			_putchar(lc);
+		for (la = 'a' la <= 'z'; la++)
+			_putchar(la);
 		_putchar('\n');
 	}
 }
