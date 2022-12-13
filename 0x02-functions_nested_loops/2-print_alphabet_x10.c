0b77d6cfe56395f7b212a450282b8802d9361749
sirtladi <kgahlishot@gmail.com>
2022-12-13 12:48:02 -0800

    task 2-print_alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alpha 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char lc;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (lc = 'a' lc <= 'z'; la++)
+			_putchar(lc);
+		_putchar('\n');
+	}
+}
