0793e788e08fa9dd82fe1917db326cb285eec115
sirtladi <kgahlishot@gmail.com>
2022-12-13 15:51:02 -0800

    task 4 is_alpha

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,10 @@
 
 /**
  * _islower - checks if a character is lowercase
- * @c: the character
+ * @c: is the char to be checked
  * Retrun: 1 if letter is lowercase, 0 if not
  */
+
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
