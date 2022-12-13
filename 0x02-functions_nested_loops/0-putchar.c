51e79463ebf64c6e8c96fbc6d4ef7161dfc31974
sirtladi <kgahlishot@gmail.com>
2022-12-13 04:44:48 -0800

    task 00 that_putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,12 @@
-#include <stdio.h>
 #include <main.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 
 int main(void)
 {
-	printf("_putchar\n")
-		return (0)
+	printf("_putchar\n");
+		return (0);
 }
