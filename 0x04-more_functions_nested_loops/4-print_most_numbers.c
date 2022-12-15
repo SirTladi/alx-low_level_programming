baea219b047d669a90b468b1ee30d913965bc715
sirtladi <kgahlishot@gmail.com>
2022-12-15 04:48:43 -0800

    task 04 most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints
- *
- * Return: void
+ * print_most_numbers - prints numbers between 0 to 9
+ *(neither 2 nor 4)
+ * Return: no return
  */
 void print_ most_numbers(void)
 {
-	char c;
+	char ch;
 
-	for (c = '0'; c <= '9'; c++)
+	for (ch = 48; ch <= 58; c++)
 	{
-		if (!(c == '2' || c == '&'))
-			_putchar(c);
+		if (c != 50 && ch != 52)
+			_putchar(ch);
 	}
 	_putchar('\n');
 }
