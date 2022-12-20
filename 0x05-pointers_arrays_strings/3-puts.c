69cd99c9a54ef4b9733a3bd25a417399c7c40941
sirtladi <kgahlishot@gmail.com>
2022-12-20 06:05:10 -0800

    task 003 updated

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,8 @@
 #include "main.h"
 
 /**
- * _puts - Write a function that prints a string, followed by a new line, to
- * stdout.
- *
- * @str: This is my input string
- *
+ * _puts - prints a string
+ * @str: string to be printed
  */
 void _puts(char *str)
 {
