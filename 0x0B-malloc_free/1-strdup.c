d5d19351d937c5f25e1656c6420563840b7fc48b
SirTladi <Kgahlishot@gmail.com>
2023-01-11 07:36:03 -0800

    task 01

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -20,3 +20,23 @@ char *_strup(char *str)
 	{
 		return (NULL);
 	}
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	nstr = malloc(sizeof(char) * (len + 1));
+
+	/*check if malloc was successful*/
+	if (nstr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		nstr[i] = str[i];
+	}
+	nstr[len] = '\0';
+	return (nstr);
+}
