b437052624827716c19d55e8651d75408f386145
SirTladi <Kgahlishot@gmail.com>
2023-01-11 08:11:41 -0800

    task one

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,31 +12,29 @@
  */
 char *_strdup(char *str)
 {
-        char *nstr;
-        unsigned int len, i;
+	char *nstr;
+	unsigned int len, i;
+	/* check is str null */
+	if (str == NULL)
+	{
+		return (NULL);
+	}
 
-        /* check is str null */
-        if (str == NULL)
-        {
-                return (NULL);
-        }
-
-        len = 0;
-        while (str[len] != '\0')
-        {
-                len++;
-        }
-        nstr = malloc(sizeof(char) * (len + 1));
-
-        /*check if malloc was successful*/
-        if (nstr == NULL)
-        {
-                return (NULL);
-        }
-        for (i = 0; i < len; i++)
-        {
-                nstr[i] = str[i];
-        }
-        nstr[len] = '\0';
-        return (nstr);
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	nstr = malloc(sizeof(char) * (len + 1));
+	/* check if malloc was successful */
+	if (nstr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		nstr[i] = str[i];
+	}
+	nstr[len] = '\0';
+	return (nstr);
 }
