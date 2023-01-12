5769cabf521f5b17a070a327b52d58c3c40f2feb
SirTladi <Kgahlishot@gmail.com>
2023-01-12 08:36:25 -0800

    task 0001

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ int _strlen(char *string)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	int num, len, i j;
+	int num, len, i, j;
 
 	num = n
 		if (s1 == NULL) /* account for NULL strings */
@@ -38,7 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (num >= _strlen(s2)) /* account for n too big */
 		num = _strlen(s2);
 
-	len = _strlen(s1) + 1; /* +1 to account for null pointer */
+	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
 
 	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
 	if (ptr == NULL)
